3ba947d45e3cbe43a4de7819cd277d4558a18e73
keshab <keshabpandey12@gmail.com>
2018-09-17 16:05:39 -0400

    My tag file in branch keshu

---
+++ tag.sh
@@ -0,0 +1,9 @@
+# Here we explain how to tag the image 
+
+docker images ( shows list of image)
+
+docker tag <name of source image> <name of destination >
+
+example: docker tag centos:6 myrepo/mycentos:version2
+
+
